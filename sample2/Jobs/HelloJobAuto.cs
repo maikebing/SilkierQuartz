f4418b02fc74443b86478d5133d37409dfddb221
MysticBoy <mysticboy@live.com>
2022-04-27 23:22:39 +0800

    测试

--- sample2/Jobs/HelloJobAuto.cs
+++ sample2/Jobs/HelloJobAuto.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace SilkierQuartz.Example.Jobs
 {
-
+ 
     [SilkierQuartz(5, "this e sq test","_hellojobauto")]
     public class HelloJobAuto : IJob
     {
@@ -18,5 +18,15 @@ namespace SilkierQuartz.Example.Jobs
         }
     }
 
-  
+    [DisallowConcurrentExecution]
+    [SilkierQuartz(5, 0, 0, Desciption = "自动下载欢迎信息")]
+    public class HelloJobAuto1 : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine($"Hello {DateTime.Now}");
+            return Task.CompletedTask;
+        }
+    }
+
 }
